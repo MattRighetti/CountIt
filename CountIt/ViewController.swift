9862b0175edf1350f15a0799fc3497be30808580
Mattia Righetti <matt95.righetti@gmail.com>
2018-09-15 12:49:43 +0200

    Initial Commit

---
+++ CountIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountIt
+//
+//  Created by Mattia Righetti on 15/09/2018.
+//  Copyright © 2018 Mattia Righetti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
